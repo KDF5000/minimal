353093894aaa62ff7e4b0a3b9be2f3465d1663de
kongdefei <kdf5000@163.com>
2018-01-20 22:54:09 +0800

    alter js

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -1,12 +1,12 @@
 var metas = document.getElementsByTagName('meta');
 var i;
 if (navigator.userAgent.match(/iPhone/i)) {
-  for (i=0;i<metas.length;i++) {
-    if (metas[i].name == "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+    for (i = 0;i < metas.length; i++) {
+        if (metas[i].name == "viewport") {
+          metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+        }
     }
-  }
-  document.addEventListener("gesturestart", gestureStart, false);
+    document.addEventListener("gesturestart", gestureStart, false);
 }
 function gestureStart() {
   for (i=0; i<metas.length; i++) {
